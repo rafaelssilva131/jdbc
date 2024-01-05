a0759cd07490c694e840ae05df00558c7d2cf7f4
rafaelssilva131 <rafael.ssilva131@gmail.com>
2024-01-05 20:25:41 -0300

    update

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,10 +35,16 @@ public class Program {
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
-		System.out.println("==== TEST 4: seller findAll ====");
+		System.out.println("==== TEST 4: seller insert ====");
 		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = "+ newSeller.getId());
+		
+		System.out.println("==== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
 	}
 		
 }
